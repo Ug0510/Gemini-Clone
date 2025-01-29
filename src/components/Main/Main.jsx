d6ff5cece8ebf8f86adb0f4a536bbb6f9549b8b6
Ug0510 <uditgg0510@gmail.com>
2025-01-29 21:05:27 +0530

    changed the placeholder of searchbox

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -35,7 +35,7 @@ const Main = () => {
 
             <div className="main-bottom">
                 <div className="search-box">
-                    <input type="text" placeholder='Enter your prompt here...' />
+                    <input type="text" placeholder='Enter a prompt here' />
                     <div>
                         <img src={assets.gallery_icon} alt="" />
                         <img src={assets.mic_icon} alt="" />
