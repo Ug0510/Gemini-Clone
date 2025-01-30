35507ea9d98ada9d6697b2acf1e7ab0668c4dd3b
Ug0510 <uditgg0510@gmail.com>
2025-01-30 15:27:38 +0530

    Added Text formatting and typing effect in the response generation

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -11,13 +11,42 @@ const ContextProvider = (props) => {
     const [loading, setLoading] = useState(false);
     const [resultData, setResultData] = useState("");
 
+    const delayPara = (index,nextWord) => 
+    {
+        setTimeout(() => {
+            setResultData((prev) => prev + nextWord);
+        }, 75 * index);
+    };
+
     const onSent = async () => {
         setResultData('');
         setLoading(true);
         setShowResult(true);
         setRecentPrompt(input);
         const response = await run(input);
-        setResultData(response);
+        let responseArr = response.split("**");
+        let newArr = "";
+
+        for(let i = 0; i < responseArr.length; i++)
+        {
+            if(i % 2 === 0)
+            {
+                newArr += responseArr[i];
+            }
+            else{
+                newArr += "<b>" + responseArr[i] + "</b>";
+            }
+        }
+
+        let newResponseArr = newArr.split("*").join("</br>");
+
+        
+        let newResponseArray = newResponseArr.split(" ");
+        for(let i = 0; i < newResponseArray.length; i++)
+        {
+            const nextWord = newResponseArray[i];
+            delayPara(i,nextWord+" ");
+        }
         setLoading(false);
         setInput('');
 
