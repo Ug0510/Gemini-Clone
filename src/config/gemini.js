1a05e950589aedcefc40c4ec745c477be546a38a
Ug0510 <85778092+Ug0510@users.noreply.github.com>
2025-01-31 02:37:58 +0530

    Update gemini.js

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -4,7 +4,7 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = 'AIzaSyAXbD-E9TA9POdzUbibyXwr39x63ZMZq80';
+const apiKey = '<To be Added>';
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
