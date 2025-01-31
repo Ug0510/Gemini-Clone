32839798f08fd39052ba9c923c990c28008cbd3c
Ug0510 <uditgg0510@gmail.com>
2025-01-31 10:35:36 +0530

    Added the key as environment variable

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -4,7 +4,7 @@ import {
   HarmBlockThreshold,
 } from "@google/generative-ai";
 
-const apiKey = '<To be Added>';
+const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
